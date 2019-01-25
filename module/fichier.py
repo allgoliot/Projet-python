14fd14a23e6c8ad1f42f23f05c8ed37fb761b158
jun <wujunxian1990@hotmail.com>
2019-01-25 12:30:16 +0100

    fichier.py fait

--- module/fichier.py
+++ module/fichier.py
@@ -1,6 +1,12 @@
 # Opération sur les fichiers 
 #créer (collaboration avec les sous-repertoires suivants)
 # developpements, tests, livraisons, bugs
+#Puis créer respectivement dans chaque sous repertoire les fichiers suivants: 
+#developpement.txt, tests.txt, livraisons.txt et bugs.txt
+#dans developpements.txt, écrire la phrase: ce repertoire contiendra vos développements
+#dans tests.txt, écrire la phrase: ce repertoire contiendra vos tests
+#dans livraisons.txt, écrire la phrase: ce repertoire contiendra vos livraisons
+#dans bugs.txt, écrire la phrase: ce repertoire contiendra les bugs rencontrés
 import os
 import os.path
 def fichiers():
@@ -10,4 +16,9 @@ def fichiers():
 		os.makedirs (chemin)
 		for i in sous_rep:
 			os.makedirs (chemin+"/"+i)
+			source = chemin+"/"+i+"/"+i+".txt"
+			fichiers = open(source, "w")
+			fichiers.write("ce repertoire contiendra vos " + i)
+			fichiers.close()
+		
 fichiers()
