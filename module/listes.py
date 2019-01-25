5b5cca048395ad8f44f1bad250b3b94afd0b98fb
allan <allan.goliot@gmail.com>
2019-01-25 13:38:38 +0100

    ajout d'un menu interractif dans lyste.py

--- module/listes.py
+++ module/listes.py
@@ -35,3 +35,57 @@ def demoliste():
 	print("je suprime tutu de ma liste")
 	supprime(ma_liste,"tutu")
 	afficher_la_liste(ma_liste)
+
+def creerliste():
+	fin=0
+	cpt=1
+	liste=[]
+	while fin == 0:
+		message="element n°"+str(cpt)+" de la liste (END to stop) : "
+		entree=input(message)
+		if entree != "END":
+			liste.append(entree)
+			cpt+=1
+		else:
+			fin=1
+	return liste
+
+def menu():
+	retour=0
+	print("---Menu de la mort !---")
+	print("1. Compter le nb d\'element d\'une liste")
+	print("2. Afficher la liste")
+	print("3. Ajouter un element en fin de liste")
+	print("4. Remplacer un element de la liste par un autre")
+	print("5. Supprimer un element de la liste")
+	print("6. Demo de liste.py")
+	print("*. Fin")
+	choix=input("Votre choix : ")
+	if choix=="1":
+		print("----")
+		liste=creerliste()
+		print(nb_elements(liste))
+		print("----")
+	elif choix=="2":
+		print("")
+	elif choix=="3":
+		print("")
+	elif choix=="4":
+		print("")
+	elif choix=="5":
+		print("")
+	elif choix=="6":
+		demoliste()
+	else:
+		print("Aucun choix")
+		print("Fin du programme")
+		retour = 1
+	return retour
+
+
+def Boucle():
+	fin=0
+	while fin == 0:
+		fin=menu()
+
+Boucle()
